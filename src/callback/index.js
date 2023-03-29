d7113363b05b58cdf2d8d9261522d67585078737
Valentina Vargas Patiño <vvargaspr@gmail.com>
2023-03-28 21:18:08 -0500

    Clase 1 - callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,18 @@
+function sum(num1, num2){
+    return num1 + num2;
+};
+
+function calc(num1, num2, callback){
+    return callback(num1, num2);
+};
+
+console.log(calc(2, 2, sum));
+
+setTimeout(function(){
+    console.log('Hola Javascripit')
+}, 5000);
+
+// Alt + 96 ``
+function gretting(name){
+    console.log(`Hola ${name}`)
+}
